152bdcb68aab615570f6f74859fbdbefd3268ab8
changzhen976 <changzhen976@gmail.com>
2022-01-07 15:02:22 +0800

    add post route

--- routes/dev.js
+++ routes/dev.js
@@ -1,4 +1,8 @@
 var express = require('express');
+const { json } = require('express/lib/response');
+var bodyParser = require('body-parser')
+
+
 var router = express.Router();
 
 var mysqlconnection = require('../mysqlconnection');
@@ -6,11 +10,22 @@ var mysqlconnection = require('../mysqlconnection');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-mysqlconnection.query('SELECT * from runoob_tbl', (err, rows, fields) => {
+  mysqlconnection.query('SELECT * from runoob_tbl', (err, rows, fields) => {
     console.log(rows)
     })
 
   res.render('dev', { title: 'dev page' });
 });
+
+
+router.post('/', bodyParser.json(),function(req,res){
+
+  var result = req.body
+
+  console.log(result)
+
+  res.render('dev',{ title: 'dev page' })
+
+})
 
 module.exports = router;
